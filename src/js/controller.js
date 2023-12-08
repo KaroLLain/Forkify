29f462ecec664a9e418d690167e3111368a81f89
KaroLLain <39537241+KaroLLain@users.noreply.github.com>
2023-12-08 14:33:12 +0100

    comm

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,15 +1,19 @@
 import * as model from './model.js';
 import recipeView from './vievs/recipeView.js';
 import searchView from './vievs/searchView.js';
+import resultsView from './vievs/resultsView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import { async } from 'regenerator-runtime';
 
+if (module.hot) {
+  module.hot.accept();
+}
+
 const contolRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
-    console.log(id);
 
     if (!id) return;
     recipeView.renderSpinner();
@@ -26,11 +30,16 @@ const contolRecipes = async function () {
 
 const controlSearchResult = async function () {
   try {
+    resultsView.renderSpinner();
+    //1. Get search query
     const query = searchView.getQuery();
     if (!query) return;
 
-    model.controlSearchResult('pizza');
-    console.log(model.state.search.results);
+    //2. Load search results
+    await model.loadSearchResults(query);
+
+    //3. Render results
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.log(err);
   }
--- src/js/vievs/searchView.js
+++ src/js/vievs/searchView.js
@@ -1,12 +1,18 @@
 class SearchView {
-  #parentEl = document.querySelector('.search');
+  _parentElement = document.querySelector('.search');
 
   getQuery () {
-    return this.#parentEl.querySelector('.search__field').value;
+    const query = this._parentElement.querySelector('.search__field').value;
+    this._clearInput();
+    return query;
+  }
+
+  _clearInput () {
+    this._parentElement.querySelector('.search__field').value = '';
   }
 
   addHandlerSearch (handler) {
-    this.#parentEl.addEventListener('submit', function (e) {
+    this._parentElement.addEventListener('submit', function (e) {
       e.preventDefault();
       handler();
     });
